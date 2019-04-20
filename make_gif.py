00d264fa39f2be5c6678805e4b92953965da9c2f
wontak <ryu071511@gmail.com>
2019-04-20 12:31:54 +0900

    make gif file

--- make_gif.py
+++ make_gif.py
@@ -12,7 +12,7 @@ def img_file_to_gif(img_file_lst, output_file_name):
 
 
 result_file = sorted(glob.glob('./result/*.*'), key = (lambda x : int(re.findall('\d+',x)[0])))
-variance_file = sorted(glob.glob('./result/*.*'), key = (lambda x : int(re.findall('\d+',x)[0])))
+variance_file = sorted(glob.glob('./variance/*.*'), key = (lambda x : int(re.findall('\d+',x)[0])))
 
 img_file_to_gif(result_file, "./result/result.gif")
 img_file_to_gif(variance_file, "./variance/variance.gif")
